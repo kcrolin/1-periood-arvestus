ffcb4ae12de02dc8578744912b37568274524c50
kcrolin <carolin.karing@khk.ee>
2014-09-30 08:47:30 +0100

    1. perioodi arvestus
    arvestustöö

---
+++ components/html-head.php
@@ -0,0 +1,8 @@
+<!-- Basic meta info -->
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<!-- Stylesheets -->
+<link rel="stylesheet" href="../stylesheets/main.css">
+
+<!-- Site title -->
+<title>SVP - Arvestustöö</title>
